2132133a7a76518b73a42d993f13b2a852d47ba2
Jason Utt <jdmagic21@gmail.com>
2019-07-08 14:28:23 -0400

    Schema.org support

---
+++ README.md
@@ -0,0 +1,14 @@
+## Description ##
+A WordPress plugin that allows you to write schema.org specific testimonials using shortcodes.
+
+## How to Use ##
+1. Download the plugin and use the following shortcode. Must include a max rating if using rating system
+`[testimonial author="Author Name" rating="2"  maxRating ="5" date="03-13-90"]Sample text testimonial![/testimonial]`
+
+## Change Log ##
+### 07-08-91 ###
+- Added max star rating attribute with new stars
+- Added schema.org tags
+
+## What I Have Learned ##
+- You cannot use camel case with shortcode attributes
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: CC Testimonials
  * Description: [testimonial author="name" rating="4" date="03-13-90"]Text[/testimonial]
- * Version: 1.0
+ * Version: 1.1
  * Author: Jason Utt
  * Author URI: www.codedcontainer
  * License: Attribution-NoDerivatives 4.0 International
@@ -16,24 +16,44 @@ wp_enqueue_style('star', plugins_url('/icons/css/star-embedded.css',__FILE__));
 wp_enqueue_style('star', plugins_url('/icons/css/star-codes.css',__FILE__));
 wp_enqueue_style('star', plugins_url('/icons/css/animation',__FILE__));
 
-
+$htmlOut; 
 //[testimonial]
 function testimonial_func( $atts, $content = null ){
 	$a = shortcode_atts(array(
 	'author' => '',
 	'rating' => 0,
+	'maxrating' => 0,
 	'date' => NULL
 ), $atts );
-	for ($i=1; $i <= $a['rating']; $i++)
-	{
-	$starIcon.="<i class='icon-star' style='color: #FFA500;'></i>";
+
+	for ($i=1; $i <= $a['rating']; $i++){
+		$starIcon.="<i class='icon-star' style='color: #FFA500;'></i>";
 	}
+	if ($a['rating'] > 0){
+		for($i=1; $i<= $a['maxrating'] - $a['rating']; $i++){
+			$starIcon.="<i class='icon-star' style='color: #bdbcbb;'></i>";
+		}
+	}	
 
 	if($a['date'] != NULL )
 	{
 		$date = " | " . $a['date'];
 	}
-	return "<blockquote style='background:url(".plugins_url( '/blockquote.png' , __FILE__ ).") 15px 20px no-repeat; padding: 15px 20px 10px 80px; margin: 10px; color: #555; font-style-italic; font-family: Georgia, serif; height: auto; font-style: italic; width: auto; box-shadow: 1px 1px 10px'>"."<p>".$content."</p>"."<p style='text-align: right;'>" . $a['author'] . $date . "<br/>" . $starIcon."</blockquote>";
+	$htmlOut .= "<blockquote itemprop='review' itemscope itemtype='http://schema.org/Review' style='background:url("; 
+	$htmlOut .= plugins_url( '/blockquote.png' , __FILE__ ); 
+	$htmlOut .= ") 15px 20px no-repeat; padding: 15px 20px 10px 80px; margin: 10px; color: #555; font-style-italic; font-family: Georgia, serif; height: auto; font-style: italic; width: auto; box-shadow: 1px 1px 10px'>"; 
+	$htmlOut .= "<p itemprop='description'>".$content."</p>"; 
+	$htmlOut .= "<p style='text-align: right;'>";
+	$htmlOut .= "<span itemprop='author'>" . $a['author'] . "</span>";
+	$htmlOut .= "<span itemprop='datePublished'>".$date ."</span><br/> ";
+	$htmlOut .= "<p itemprop='reviewRating' itemscope itemtype='http://schema.org/Rating' style='text-align: right;'>";
+	$htmlOut .= "<meta itemprop='worstRating' content='0'>";
+	$htmlOut .= "<span itemprop='ratingValue' style='display: none'>".$a['rating']."</span>";
+	$htmlOut .= "<span itemprop='bestRating' style='display:none'>".$a['maxrating']."</span>";
+	$htmlOut .= $starIcon."</p>";
+	$htmlout .= "</blockquote>"; 
+
+	return $htmlOut;
 }
 add_shortcode( 'testimonial', 'testimonial_func' );
 
